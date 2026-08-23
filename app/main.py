0eddf7b6e4f266fa199ddcf799c3ecd481ce38d9
SSSSS0828 <1816318543@qq.com>
2026-08-23 15:08:47 +0800

    inject runtime environment from github secret

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
+import os
+
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
 
+# 如果系统提供了 APP_ENV 环境变量，则使用它的值，否则默认为 development
+APP_ENV = os.getenv("APP_ENV", "development")
+
 @app.get("/")
 def index():
     return {
         "service": "devops-lab",
         "status": "ok",
-        "version": "v3"
+        "version": "v3",
+        "environment": APP_ENV
+
     }
 
 @app.get("/health")
